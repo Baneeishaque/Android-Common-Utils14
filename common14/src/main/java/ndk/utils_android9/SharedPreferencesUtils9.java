27f00de0dc004e853e02ff038abf7700d9b0155a
Banee Ishaque K <baneeishaque@gmail.com>
2024-01-06 17:03:34 +0530

    Typo

--- common14/src/main/java/ndk/utils_android9/SharedPreferencesUtils9.java
+++ common14/src/main/java/ndk/utils_android9/SharedPreferencesUtils9.java
@@ -3,7 +3,7 @@ package ndk.utils_android9;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public class SharedPreferencesUtils extends ndk.utils_android1.SharedPreferencesUtils1 {
+public class SharedPreferencesUtils9 extends ndk.utils_android1.SharedPreferencesUtils1 {
 
     public static void removeKeyValuePair(Context applicationContext, String applicationName, String key) {
 
