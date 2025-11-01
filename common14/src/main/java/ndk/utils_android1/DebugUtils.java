e52b72897b97bd0964d688f5de8b276dc11732d9
Banee Ishaque K <baneeishaque@gmail.com>
2025-11-01 18:42:08 +0530

    Enforces non-null Context parameter
    Improves code robustness and aids static analysis by explicitly marking the `Context` parameter as non-null.

--- common14/src/main/java/ndk/utils_android1/DebugUtils.java
+++ common14/src/main/java/ndk/utils_android1/DebugUtils.java
@@ -3,9 +3,11 @@ package ndk.utils_android1;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 
+import androidx.annotation.NonNull;
+
 public class DebugUtils {
 
-    public static boolean isDebugBuild(Context currentApplicationContext) {
+    public static boolean isDebugBuild(@NonNull Context currentApplicationContext) {
 
         return (0 != (currentApplicationContext.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE));
     }
